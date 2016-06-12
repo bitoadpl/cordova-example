32fdc41f191a4f14ac320eec8c580c86de450da9
bitoadpl <services@bitoad.com>
2016-06-12 20:57:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-
-document.getElementById('msg').innerHTML = 'ok';
-
 console.log('index.js init');
-
-var zeroconf = cordova.plugins.zeroconf;
-
-zeroconf.register('_http._tcp.local.', 'Android Phone', 80, {
-    'foo' : 'bar'
-});
 
